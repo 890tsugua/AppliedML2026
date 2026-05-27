1ae22668f4249c825ffc713208ac424539016cdd
890tsugua <aubhdk@gmail.com>
2026-05-27 11:19:30 +0200

    Added trainable layers option

--- country_cnn/src/model.py
+++ country_cnn/src/model.py
@@ -5,15 +5,23 @@ from torchvision import models
 def make_model(
     model_name="resnet34",
     num_classes=85,
-    pretrained=True
+    pretrained=True,
+    trainable_layers=1
 ):
     """
     Create a torchvision ResNet model.
 
     Args:
-        model_name: resnet18, resnet34, resnet50, resnet101 or resnet152. Default resnet34.
-        num_classes: number of output classes. Default 85.
-        pretrained: whether to use ImageNet pretrained weights. Default True
+        model_name: resnet18, resnet34, resnet50, ...
+        num_classes: number of output classes
+        pretrained: use ImageNet pretrained weights
+        trainable_layers:
+            0 = only fc
+            1 = fc + layer4
+            2 = fc + layer4 + layer3
+            3 = fc + layer4 + layer3 + layer2
+            4 = fc + layer4 + layer3 + layer2 + layer1
+            5 = full model trainable
     """
 
     # get model constructor dynamically
@@ -27,7 +35,24 @@ def make_model(
 
     # replace classifier
     in_features = model.fc.in_features
-    model.fc = nn.Linear(in_features, num_classes) 
-    # fc "fully connected" layer is the last layer of the model, which does classification.
+    model.fc = nn.Linear(in_features, num_classes)
+
+    # freeze everything first
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # layers ordered from last to first
+    layers_to_unfreeze = [
+        model.fc,
+        model.layer4,
+        model.layer3,
+        model.layer2,
+        model.layer1,
+    ]
+
+    # unfreeze requested number of layers
+    for layer in layers_to_unfreeze[:trainable_layers + 1]:
+        for param in layer.parameters():
+            param.requires_grad = True
 
     return model
